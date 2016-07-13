7d6f88763e42344df56b074f37cf4e2e8e350ae6
“[josue <[josuerojas.rojas@gmail.com]>
2016-07-12 22:17:26 -0400

    none
    none

--- app/src/main/java/comjosuerojasrojas/httpsgithub/simon/MainActivity.java
+++ app/src/main/java/comjosuerojasrojas/httpsgithub/simon/MainActivity.java
@@ -2,6 +2,7 @@ package comjosuerojasrojas.httpsgithub.simon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClickColor(View view) {
+
+    }
 }
+
