4559540a2c48ffcc703c500361f920e67276c680
edmorales94 <edmorales94@gmail.com>
2016-07-14 11:16:40 -0400

    Add files via upload

--- edgar/MainActivity.java
+++ edgar/MainActivity.java
@@ -1,42 +1,187 @@
-package com.morales.edgar.simonsays;
+package comjosuerojasrojas.httpsgithub.simon;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.util.Log;
-import android.widget.TextView;
-
-import java.util.Stack;
-import java.util.Random;
+import android.os.Handler;
+import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public Stack inputStack = new Stack();
-    Button populate;
+
+    //constants
+    private static int numButtons = Constants.numButtons;
+    private static int[] buttonID = new int[numButtons];
+    private final Handler handler = new Handler();
+    Button reset;
+    // private boolean firstTurn = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        populate = (Button) findViewById(R.id.fillwithrandomnumbers);
-        inputStack.removeAllElements();
-        populate.setOnClickListener(
+
+        //get all button id number
+        //might throw error if button doesn't exist
+        for (int i = 0; i < numButtons; i++) {
+            buttonID[i] = (getResources().getIdentifier("button_" + i, "id", getPackageName()));
+            ((Button) findViewById(buttonID[i])).setOnTouchListener(listener);
+            //populate constants
+            Constants.ID[i] = ((Button) findViewById(buttonID[i])).getId();
+            Constants.idToColor.put(((Button)findViewById(buttonID[i])).getId(),Constants.colorPressed[i]);
+            Constants.idToColorDef.put(((Button)findViewById(buttonID[i])).getId(),Constants.colorDefault[i]);
+
+        }
+        //computer turn (always first)
+        computerAI.addValues();
+        computerTurn();
+
+        //seekbar and it's function (messy for now)
+        ((SeekBar)findViewById(R.id.seekBar)).setProgress(0);
+        ((SeekBar)findViewById(R.id.seekBar)).setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Constants.delay = 1100 - progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        reset = (Button)findViewById(R.id.reset);
+
+        reset.setOnClickListener(
                 new Button.OnClickListener(){
                     public void onClick(View v){
-                        randomStack();
-                        /*for(int i = 0; i < inputStack.size(); i++){
-                            Log.d("simonSays", "element in stack: " + inputStack.elementAt(i));
-                            TextView stack = (TextView) findViewById(R.id.textView);
-                            stack.setText(inputStack.elementAt(i)+",");
-                        }*/
+                        computerAI.reset();
+                        Toast.makeText(getApplicationContext(),"Game has been resetted",Toast.LENGTH_LONG).show();
                     }
                 }
         );
     }
 
-    public void randomStack(){
-        Random random = new Random();
-        int randomNumber = random.nextInt(4);
-        inputStack.push(randomNumber);
+
+    //onTouchListener is used to disable buttons when one is press 
+    View.OnTouchListener listener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            Long time = System.currentTimeMillis();
+
+            if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                Log.d("inside","disable buttons");
+                disableButtons(v.getId());
+
+                //return true;
+            }
+            if(event.getAction() == MotionEvent.ACTION_UP) {
+              //  Button button = (Button)findViewById(v.getId());
+                //fix the button not showing the color. should be cleaned later
+                /*
+                boolean whileT = true;
+                while(System.currentTimeMillis() - time < Constants.delay){
+                    if(whileT){
+                        whileT=false;
+                        Log.d("inside","change color");
+                    }
+                    button.setBackgroundColor(Color.parseColor(
+                            Constants.idToColor.get(new Integer(button.getId()))));
+                }
+
+                button.invalidate();
+                //check if right
+                button.setBackgroundColor(Color.parseColor(
+                        Constants.idToColorDef.get(new Integer(button.getId()))));
+                        */
+                //add to pattern
+                computerAI.addValues();
+
+
+
+                //display the computer turn
+                computerTurn();
+
+                //just in case
+                enableButtons();
+            }
+
+            return false;
+        }
+    };
+
+
+
+
+
+    //function to diasble
+    public void disableButtons(int id) {
+        for (int i = 0; i < buttonID.length; i++) {
+            if (buttonID[i] != id)
+                ((Button) findViewById(buttonID[i])).setEnabled(false);
+        }
     }
-}
+
+    public void enableButtons() {
+        for (int i = 0; i < buttonID.length; i++) {
+            ((Button) findViewById(buttonID[i])).setEnabled(true);
+        }
+        Log.d("inside","enablebuttons");
+    }
+
+
+    //handle delay from computer turn and show the pattern
+    public void computerTurn(){
+
+        final Button button = (Button)findViewById(Constants.ID[computerAI.computerValues.get(computerAI.current)]);
+        //change to 'clicked' button color
+        button.setBackgroundColor(Color.parseColor(
+                Constants.idToColor.get(new Integer(button.getId()))));
+        //disable buttons
+        disableButtons(-1);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //computerAI.getNext();
+
+                //set default color (for computer)
+                    button.setBackgroundColor(Color.parseColor(
+                            Constants.idToColorDef.get(new Integer(button.getId()))));
+                Log.d("inside","button change");
+                //run until current gets to last
+                computerAI.getNext();
+                if(computerAI.current != computerAI.end) {
+                    computerTurn();
+                }
+                else{
+                    enableButtons();
+                    computerAI.getNext();
+                }
+            }},Constants.delay);
+
+
+        }
+
+
+
+    public void multiplayer(){};
+
+
+
+
+    }
+
+
+
+
+
